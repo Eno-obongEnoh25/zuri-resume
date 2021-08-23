504f5cdd8d6ff4bc46bc7e3d03cd76a3a6a7cfc2
enoobongenoh25 <enoobongenoh25@gmail.com>
2021-08-24 00:20:14 +0100

    added e-comerce link

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -61,6 +61,7 @@
                 <h4>Links to Projects:</h4>
                 <h5><a href="https://github.com/Eno-obongEnoh25" target="_blank">Github</a></h5>
                 <h5><a href="https://blog-enoh.herokuapp.com/blog" target="_blank">Blog app</a></h5>
+                <h5><a href="https://e-commerce-enoh.herokuapp.com/welcome" target="_blank">E-commerce app</a></h5>
             </div>
 
             <div class="text-center"><h3>Please fill in the form below to contact me</h3></div>
